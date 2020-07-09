22d4746a860fb131ac596b4c2de6b4859683924d
nikhilmishra1998 <mishra123nikhil@gmail.com>
2020-07-09 09:39:13 +0530

    Adding logic of verifyUserName API

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -20,12 +20,41 @@ exports.registerUser = async (req, res, next) => {
     devices: [],
   };
 
-  data.devices.push(data.currentDevice);
+  /* data.devices.push(data.currentDevice);
 
   try {
     user = new User(data);
     console.log(await user.save());
   } catch (error) {
     console.log(error);
+  } */
+};
+
+exports.postRegisterUser = async (req, res, next) => {
+  const requestData = req.body;
+  res.json({ message: requestData });
+};
+
+exports.postVerifyUserName = async (req, res, next) => {
+  const userName = req.body.userName;
+
+  // Validating the post data
+  if (typeof userName !== "string" || !userName) {
+    return res.json({ error: "userName is required" });
+  }
+
+  try {
+    const param = { userName: userName };
+    User.findOne(param, function (error, user) {
+      if (error) {
+        console.log(error);
+        return res.json({ error: "There is some error with this userName" });
+      }
+      console.log(user);
+      return res.json({ message: user });
+    });
+  } catch (error) {
+    console.log(error);
+    return res.json({ error: "There is some error with this userName" });
   }
 };
